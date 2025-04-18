d0de01574cd9ed46b2d9e9ce1a5b018be1b97cc5
Joe <joebbaney@gmail.com>
2025-04-18 11:05:03 -0500

    made some styling changes, changed eventListener to drag instead of hover

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const body = document.querySelector('body')
 
-
 const containerDiv = document.createElement('div')
 const bottomDiv = document.createElement('div')
 const header = document.createElement('h1')
+const paragraph = document.createElement('p')
 const smallButton = document.createElement('button')
 const mediumButton = document.createElement('button')
 const largeButton = document.createElement('button')
@@ -11,11 +11,13 @@ const xLargeButton = document.createElement('button')
 const bottomHeader = document.createElement('h3')
 const resetButton = document.createElement('button')
 
+
 body.appendChild(header)
+body.appendChild(paragraph)
 body.appendChild(containerDiv)
 body.appendChild(bottomDiv)
-
 bottomDiv.append(bottomHeader, smallButton, mediumButton, largeButton, xLargeButton, resetButton)
+
 
 containerDiv.classList.add('containerDiv')
 header.classList.add('header')
@@ -23,6 +25,7 @@ bottomDiv.classList.add('bottomDiv')
 resetButton.classList.add('resetButton')
 
 header.textContent = 'Etch-A-Sketch'
+paragraph.textContent = 'Try dragging your mouse to start coloring!'
 bottomHeader.textContent = 'Change the size of your Etch-A-Sketch layout!'
 smallButton.textContent = 'Small'
 mediumButton.textContent = 'Medium'
@@ -81,10 +84,27 @@ const getRandomColor = () => {
 }
 
 function setUpColors() {
+    let isMouseDown = false
     const eachSquare = document.querySelectorAll('.square')
+
+    const changeColor = square => {
+        square.style.backgroundColor = getRandomColor()
+    }
+
     eachSquare.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = getRandomColor()
+        square.addEventListener('mousedown', () => {
+            isMouseDown = true
+            changeColor(square)
+        })
+
+        square.addEventListener('mousemove', () => {
+            if (isMouseDown) {
+                changeColor(square)
+            }
+        })
+
+        square.addEventListener('mouseup', () => {
+            isMouseDown = false
         })
     })
 }
